76b74bc08e2d678dbb26e79aea3e66e417e98e28
madpang <133752527+madpang@users.noreply.github.com>
2025-08-31 01:31:20 +0900

    Modify the calling signature of the tone burst generation function
    Instead of returning a handle that takes a time series to generate the burst, it now accepts that time series as argument and directly returns the digital burst.

--- fToneBurst.m
+++ fToneBurst.m
@@ -1,24 +1,24 @@
 %{
-	@brief: Generate a function handle for Gaussian modulated toneburst.
-	@usage: func = fToneBurst(amp, fc, dur)
+	@brief: Generate Gaussian modulated toneburst.
+	@usage: ext = fToneBurst(amp, fc, dur, tvec)
 
 	@param[out]:
-	- func: a function handle that takes time vector as input and returns the toneburst signal.
+	- ext: Gaussian modulated sinusoid sequence.
 	@param[in]:
 	- amp: amplitude of the toneburst (sinusoid) to be modulated, scalar.
 	- fc: central frequency of the sinusoid wave, scalar.
 	- dur: duration of the modulated pulse, scalar.
+	- tvec, time vectors
 
 	@details:
 	- The toneburst is created by multiplying a sinusoid with a rectangular window and Gaussian weight.
 	- The Gaussian weight provides smooth tapering to reduce spectral leakage.
-	- Naming convention is unified with UT_2022_12_25_a.nb of stack 2023-01-08-a.
 	- No argument validation is performed.
 
 	@example:
 	```
 	fc = 2.5e+6; dur = 4/fc; tvec = 1/(31.25e+6) * (0:1:100);
-	pulse = feval(fToneBurst(100, fc, dur), tvec);
+	pulse = fToneBurst(100, fc, dur, tvec);
 	plot(pulse);
 	```
 
@@ -28,35 +28,37 @@
 	@date: [created: 2019-06-15, updated: 2023-05-29]
 %}
 
-function func = fToneBurst(amp, fc, dur)
+function ext = fToneBurst(amp, fc, dur, tvec)
 	
 	% Continuous single wave function
-	cwFun = fSinusoid(amp, fc, 0);
+	cwFun = Sinusoid(amp, fc, 0);
 
 	% Window function
-	winFun = fRecWin(dur);
+	winFun = RecWin(dur);
 
 	% Gaussian weight
-	weightFun = fGaussianWeight(dur/2, 0.01);
+	weightFun = GaussianWeight(dur/2, 0.01);
 
-	func = @(tvec) cwFun(tvec) .* winFun(tvec) .* weightFun(tvec);
+	func = @(t) cwFun(t) .* winFun(t) .* weightFun(t);
+
+	ext = func(tvec);
 
 end
 
 %% SUB FUNCTIONS
 
 % ---
-function func = fSinusoid(amp, fc, phi)
+function func = Sinusoid(amp, fc, phi)
 
 	omega = 2*pi*fc;
-	func = @(tvec) amp * sin(omega * tvec + phi);
+	func = @(t) amp * sin(omega * t + phi);
 
 end
 
 % ---
-function func = fRecWin(dur)
+function func = RecWin(dur)
 
-	func = @(tvec) (tvec >= 0 & tvec <= dur) * 1;		
+	func = @(t) (t >= 0 & t <= dur) * 1;
 
 end
 
@@ -78,7 +80,7 @@ end
 			- c is determined when value at a specific point $x_0$ is desired, usually at g(0)
 		- Refer to: <https://en.wikipedia.org/wiki/Normal_distribution>
 %}
-function func = fGaussianWeight(b, par)
+function func = GaussianWeight(b, par)
 
 	c2 = -b^2 / (2*log(par));
 	func = GaussianFcn(1, b, sqrt(c2));
@@ -87,7 +89,7 @@ end
 
 % ---
 function func = GaussianFcn(a, b, c)
-	
-	func = @(tvec) a * exp(-(tvec - b).^2 / (2 * c^2));
+
+	func = @(t) a * exp(-(t - b).^2 / (2 * c^2));
 
 end
